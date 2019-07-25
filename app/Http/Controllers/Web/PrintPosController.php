dc86be7d1c70c1c31146f0e17081565081cd06ab
daruiza <daruiza@gmail.com>
2019-07-24 22:28:29 -0500

    print Ok 30%

--- app/Http/Controllers/Web/PrintPosController.php
+++ app/Http/Controllers/Web/PrintPosController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Web;
 
 
 use App\Model\Core\Table;
+use App\Model\Core\Store;
 use App\Model\Core\Order;
+use App\Model\Core\City;
+use App\Model\Core\Department;
 
 
 use Illuminate\Http\Request;
@@ -25,6 +28,7 @@ class PrintPosController extends Controller
     {
     	$table = Table::find($request->input('table_id'));        
         $service = $table->tableServiceOpen()->first();
+        $store = Store::find($table->store_id);
 
     	$array = array();    	
         foreach($request->input() as $key=>$value){
@@ -41,20 +45,55 @@ class PrintPosController extends Controller
             $order = Order::find($key);                    
         }
 
-        dd($request->input());
+        
 
-        //$connector = new WindowsPrintConnector("TM-T20");
-        //$printer = new Printer($connector);
-        $connector = new FilePrintConnector("/dev/usb/lp2");
-        $printer = new Printer($connector);
-        $printer->text("Hello World!\n");
-        $printer->setJustification(Printer::JUSTIFY_LEFT);
-        $printer->text("jjiiji\n");
-        $printer->setJustification(Printer::JUSTIFY_RIGHT);
-        $printer->text("jajaj\n");
-        $printer->feed(2);
-        $printer->cut();
-        $printer->pulse();
-        $printer->close();
+        try {            
+            
+            dd($array);
+
+            //$connector = new WindowsPrintConnector("TM-T20");
+            //$printer = new Printer($connector);
+            $connector = new FilePrintConnector("/dev/usb/lp2");
+            $printer = new Printer($connector);
+
+            $printer->setJustification(Printer::JUSTIFY_CENTER);            
+                        
+            $printer->bitImage(EscposImage::load(
+                'users/'.\Auth::user()->id.'/stores/'.$store->logo,
+                false
+                )
+            );
+            $printer->feed();
+
+            $printer->text(strtoupper($store->name)."\n");            
+            $printer->text(strtoupper($store->description)."\n");
+            $printer->text(City::find($store->city)->name.' - '.Department::find($store->department)->name."\n");
+            
+            $printer->text(strtoupper($store->adress)."\n");
+            $printer->feed();
+
+            $printer->text("RESPONSABLE DE IVA\n");
+            $printer->text("RANGOS DE FACTURACIÓN: E 1 HASTA E 500000\n");
+            $printer->text("RESOLUCIÓN DIAN 18762014208677 DE 26-04-2019\n");                
+            $printer->feed(2);
+            
+            $printer->text("____________________________________________\n");
+            $printer->text("FACTURA DE VENTA: E ".$service->number."\n");
+            $printer->text("FECHA:  ".$service->date_open."\n");
+            $printer->text("____________________________________________\n");
+            $printer->feed();
+
+            $printer->setJustification(Printer::JUSTIFY_LEFT);
+            
+            $printer->setJustification(Printer::JUSTIFY_RIGHT);
+            
+            $printer->feed(2);
+            $printer->cut();
+            $printer->pulse();
+            $printer->close();
+        }catch (Exception $e) {
+
+            $printer->text($e->getMessage() . "\n");
+        }
     }
 }
