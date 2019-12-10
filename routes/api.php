879fcf43a0a0fcae366687827686439fad4f2cf1
daruiza <daruiza@gmail.com>
2019-12-10 05:49:23 -0500

    total config pastport

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,13 @@ Route::get('/test',function(){
     return response('{"hello": "Test API"}', 200)->header('Content-Type', 'application/json');
 });
 
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+  
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+
